ce649b0771e16811a030d22ce8627ff09053fd71
sakthi <sakthisasivelar@gmail.com>
2024-10-04 14:22:52 +0530

    added link to logo

--- src/Components/Logo/Logo.tsx
+++ src/Components/Logo/Logo.tsx
@@ -1,11 +1,12 @@
+import { Link } from 'react-router-dom';
 import Tripit from '../../assets/Logo/logo-tripit.svg';
 import Styles from './Logo.module.css'
 
 const Logo = () => {
   return (
-    <div className={Styles.container}>
+    <Link to='/' className={Styles.container}>
         <img src={Tripit} alt="Tripit" />
-    </div>
+    </Link>
   )
 }
 
